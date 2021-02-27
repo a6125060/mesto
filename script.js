21b8ff8e06eeed5c3d7b71fd7117b16c61620129
Denis Spitsyn <a6125060@yandex.ru>
2021-02-27 21:52:15 +0300

    запилил началку

--- script.js
+++ script.js
@@ -1,62 +1,20 @@
 document.querySelector('.card__like').addEventListener('click', function() {
-    document.querySelector('.card__like').classList.toggle('card__black-like');
+    document.querySelector('.card__like').classList.toggle('card__blackLike')
 });
 
 
 
 
 
-let popup = document.querySelector('.popup')
-let editButton = document.querySelector('.edit-button')
-let closeButton = document.querySelector('.popup__close')
+let popup = document.querySelector('.popup');
+let editButton = document.querySelector('.edit-button');
+let closeButton = document.querySelector('.popup__close');
 
 
 function togglePopup() {
     popup.classList.toggle('popup__opened')
-}
+};
 
-editButton.addEventListener('click', togglePopup)
+editButton.addEventListener('click', togglePopup);
 
-closeButton.addEventListener('click', togglePopup)
-
-
-
-
-
-
-
-/* 
-let like = document.querySelector('.card__like')
-
-function toggleLike() {
-    like.classList.toggle('card__black-like')
-}
-
-like.addEventListener('click', toggleLike)
-
-
-
-
-document.querySelector('.edit-button')
-
-
-
-alert('Привет, мир!');
-console.log('Hello, world!')
-
-
-
-function showEditPopup() {
-    popup.classList.add('popup__opened')
-}
-
-function closeEditPopup() {
-    popup.classList.remove('popup__opened')
-
-}
-
-editButton.addEventListener('click', showEditPopup)
-
-closeButton.addEventListener('click', closeEditPopup)
-d
-*/
+closeButton.addEventListener('click', togglePopup);
