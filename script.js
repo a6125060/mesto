6c8eabe873afdfbe61a68733358615ea4c7a594d
Denis Spitsyn <a6125060@yandex.ru>
2021-02-28 09:18:50 +0300

    сделал редактируемый профиль

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-document.querySelector('.card__like').addEventListener('click', function() {
-    document.querySelector('.card__like').classList.toggle('card__black');
+/* редактирование профиля */
+
+let popup = document.querySelector('.popup__name');
+let popup.value = 'Жак-Ив Кусто';
+
+document.querySelector('.popup').addEventListener('submit', function(event) {
+    event.preventDefault();
 });
 
+document.querySelector('.popup__window').addEventListener('submit', function() {
+    alert(document.querySelector('.popup__name').value);
+    document.querySelector('.profile-info__title').textContent = (document.querySelector('.popup__name').value);
+});
 
-
-
-
-/*
- 
-
+/* открытие/закрытие popup */
 
 let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.edit-button');
@@ -23,6 +27,28 @@ editButton.addEventListener('click', togglePopup);
 
 closeButton.addEventListener('click', togglePopup);
 
+
+
+
+/* 
+
+
+
+
+
+
+лайки 
+
+document.querySelector('.card__like').addEventListener('click', function() {
+    document.querySelector('.card__like').classList.toggle('card__black');
+});
+
+
+
+
+
+
+архив
 
 
 
