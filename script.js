e4760fe3e313db25be4ab57bf6056de260791675
Denis Spitsyn <a6125060@yandex.ru>
2021-02-28 09:21:33 +0300

    сохранить теперь закрывает popup

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
-/* редактирование профиля */
+/* редактирование профиля
 
 let popup = document.querySelector('.popup__name');
 let popup.value = 'Жак-Ив Кусто';
 
+let popup = document.querySelector('.popup__name');
+let popup.value = 'Жак-Ив Кусто';
+
+*/
 document.querySelector('.popup').addEventListener('submit', function(event) {
     event.preventDefault();
 });
 
 document.querySelector('.popup__window').addEventListener('submit', function() {
-    alert(document.querySelector('.popup__name').value);
     document.querySelector('.profile-info__title').textContent = (document.querySelector('.popup__name').value);
 });
+
+document.querySelector('.popup__window').addEventListener('submit', function() {
+    document.querySelector('.profile-info__subtitle').textContent = (document.querySelector('.popup__job').value);
+});
+
 
 /* открытие/закрытие popup */
 
 let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.edit-button');
 let closeButton = document.querySelector('.popup__close');
+let closeButton2 = document.querySelector('.popup__save');
 
 
 function togglePopup() {
@@ -27,7 +36,7 @@ editButton.addEventListener('click', togglePopup);
 
 closeButton.addEventListener('click', togglePopup);
 
-
+closeButton2.addEventListener('click', togglePopup);
 
 
 /* 
