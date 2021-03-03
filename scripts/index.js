1041dabb322f55f5ff8328539a0c21036488a1eb
Denis Spitsyn <a6125060@yandex.ru>
2021-03-03 11:39:52 +0300

    поправил Js и размеры и БЭМ

--- scripts/index.js
+++ scripts/index.js
@@ -8,12 +8,17 @@ let submitButton = document.querySelector('.popup__save');
 let formSubmit = document.querySelector('.popup__window');
 let firstName = document.querySelector('.profile-info__title');
 let job = document.querySelector('.profile-info__subtitle');
-let inputFirstName = document.querySelector('.popup__name');
-let inputJob = document.querySelector('.popup__job');
+let inputFirstName = document.querySelector('.popup__input_field_name');
+let inputJob = document.querySelector('.popup__input_field_job');
 
-/* получаем в инпуты формы предустановки*/
-inputFirstName.value = firstName.textContent;
-inputJob.value = job.textContent;
+
+
+
+/* получаем в инпуты данные со странички*/
+function putName() {
+    inputFirstName.value = firstName.textContent;
+    inputJob.value = job.textContent;
+};
 
 
 /* открытие/закрытие popup */
@@ -21,25 +26,23 @@ function togglePopup() {
     popup.classList.toggle('popup_active_true')
 };
 
+/* обнуляем предустановки для event */
 function preventDefaultEvent(event) {
     event.preventDefault();
 }
 
-function funInputFirstName() {
+function funInputProfile() {
     firstName.textContent = inputFirstName.value;
-}
-
-function funInputJob() {
     job.textContent = inputJob.value;
 }
 
 /* открытие/закрытие popup */
 editButton.addEventListener('click', togglePopup);
+editButton.addEventListener('click', putName);
 closeButton.addEventListener('click', togglePopup);
 submitButton.addEventListener('click', togglePopup);
 
 
 /* редактирование профиля popup */
 formSubmit.addEventListener('submit', preventDefaultEvent);
-formSubmit.addEventListener('submit', funInputFirstName);
-formSubmit.addEventListener('submit', funInputJob);
+formSubmit.addEventListener('submit', funInputProfile);
